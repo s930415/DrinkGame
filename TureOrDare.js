7a9a1f1e245bf5ebb19739db6d142aa15e9e684e
tkb0003213 <tkb0003213@email>
2019-02-15 17:51:20 +0800

    更新版

--- TureOrDare.js
+++ TureOrDare.js
@@ -1,19 +1,23 @@
-$('#add-people').click(function(){
-    $('.people-area').html($('.people-area').html()+$('.new-people-example').html());
-    initSexButtom();
-});
-initSexButtom();
 function initSexButtom(){
     $('.people .chose-sex i').click(function(){
         this.parentElement.parentElement.getElementsByTagName('i')[0].classList.remove('sex-checked');
         this.parentElement.parentElement.getElementsByTagName('i')[1].classList.remove('sex-checked');
         this.classList.remove('fa-lg');
         this.classList.add('sex-checked');
+        this.parentElement.parentElement.getElementsByTagName('input')[1].value = this.parentElement.dataset.sex;
     });
     $('.people .remove-sex').click(function(){
         this.parentElement.remove();
     });
 }
+
+initSexButtom();
+
+$('#add-people').click(function(){
+    $('.people-area').html($('.people-area').html()+$('.new-people-example').html());
+    initSexButtom();
+});
+
 $('.setting > a').click(function(){
     console.log(this.dataset.link);
     $('.area').hide();
