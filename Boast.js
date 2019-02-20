6099bb58dad533f2cb397f772a56f536ea12c998
tkb0003213 <tkb0003213@email>
2019-02-20 10:10:02 +0800

    大話骰加入

---
+++ Boast.js
@@ -0,0 +1,35 @@
+function point(){
+	return Math.floor(Math.random()*6)+1;
+}
+
+function shake(){
+    var dices = $('i');
+    for(let i = 0 ; i < dices.length;i++){
+        //dices[i].classList.remove('fa-dice');
+        dices[i].classList.add('fas');
+        let point = Math.floor(Math.random()*6)+1;
+        var dice = '<i class="fas ';
+        var newdice = document.createElement('i');
+        newdice.classList.add('fas');
+        switch(point){
+            case 1:
+                $('i')[i].classList.value ='fas fa-dice-one fa-3x';
+                break;
+            case 2:
+                $('i')[i].classList.value ='fas fa-dice-two fa-3x';
+                break;
+            case 3:
+                $('i')[i].classList.value ='fas fa-dice-three fa-3x';
+                break;
+            case 4:
+                $('i')[i].classList.value ='fas fa-dice-four fa-3x';
+                break;
+            case 5:
+                $('i')[i].classList.value ='fas fa-dice-five fa-3x';
+                break;
+            case 6:
+                $('i')[i].classList.value ='fas fa-dice-six fa-3x';
+                break;
+        }
+    }
+}
