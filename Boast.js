51ea55640caab602d4606a3c52a0e4d779e028e4
tkb0003213 <tkb0003213@email>
2019-03-12 12:05:13 +0800

    骰子 動畫

--- Boast.js
+++ Boast.js
@@ -1,35 +1,22 @@
-function point(){
+var point = ['one','two','three','four','five','six']
+var dices = $('i');
+
+function randomPoint(){
 	return Math.floor(Math.random()*6)+1;
 }
 
-function shake(){
-    var dices = $('i');
-    for(let i = 0 ; i < dices.length;i++){
-        //dices[i].classList.remove('fa-dice');
-        dices[i].classList.add('fas');
-        let point = Math.floor(Math.random()*6)+1;
-        var dice = '<i class="fas ';
-        var newdice = document.createElement('i');
-        newdice.classList.add('fas');
-        switch(point){
-            case 1:
-                $('i')[i].classList.value ='fas fa-dice-one fa-4x';
-                break;
-            case 2:
-                $('i')[i].classList.value ='fas fa-dice-two fa-4x';
-                break;
-            case 3:
-                $('i')[i].classList.value ='fas fa-dice-three fa-4x';
-                break;
-            case 4:
-                $('i')[i].classList.value ='fas fa-dice-four fa-4x';
-                break;
-            case 5:
-                $('i')[i].classList.value ='fas fa-dice-five fa-4x';
-                break;
-            case 6:
-                $('i')[i].classList.value ='fas fa-dice-six fa-4x';
-                break;
-        }
+function shake (){
+    for(let i = 0 ; i < dices.length ; i++){
+        dices[i].classList.value ='fas fa-dice-'+point[randomPoint()-1]+' fa-4x';
     }
 }
+
+function remove(){
+    if(dices.length === 0 ){
+        dices = $('i');
+    }else{
+        dices.splice(0,1);
+    }
+}
+
+var si = setInterval(shake,100);
